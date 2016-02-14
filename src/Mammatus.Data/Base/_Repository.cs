5aa94c1a12d1624f0ac1055da4704b473e0f43a4
DevWorker55 <omnis.devworks@gmail.com>
2016-02-13 23:56:37 -0500

    Commit Log:
    Adding initial projects and baseline structure.

---
+++ src/Mammatus.Data/Base/_Repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mammatus.Data
+{
+    public abstract class _Repository
+    {
+
+    }
+}
---
+++ src/Mammatus.Data/Repository/GenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mammatus.Data
+{
+    public class GenericRepository<TEntity> : _Repository where TEntity : class
+    {
+
+    }
+}
