cb1ccc964b4659e0434d673b22944020122db275
devworker55 <omnis.devworks@gmail.com>
2016-09-20 18:09:12 -0400

    Addedd ServicerModel.Security project.

---
+++ src/Mammatus.ServiceModel.Security/Authentication/MsSecurityTokenManager.cs
@@ -0,0 +1,21 @@
+﻿using System.IdentityModel.Selectors;
+using System.ServiceModel;
+
+namespace Mammatus.ServiceModel.Security.Authentication
+{
+    public class MsSecurityTokenManager : ClientCredentialsSecurityTokenManager
+    {
+        public MsTokenSerializerSettings CustomTokenSerializerSettings { get; set; }
+
+        public MsSecurityTokenManager(MsCredentials cred)
+            : base(cred)
+        {
+            CustomTokenSerializerSettings = new MsTokenSerializerSettings();
+        }
+
+        public override SecurityTokenSerializer CreateSecurityTokenSerializer(SecurityTokenVersion version)
+        {
+            return new MsTokenSerializer(CustomTokenSerializerSettings);
+        }
+    }
+}
