338180fc294ba96edf21bee60012b86925398674
devworker55 <omnis.devworks@gmail.com>
2016-10-22 19:24:23 -0400

    Minor integration projects updates.

--- src/Mammatus.ServiceModel.Autofac/Class1.cs
+++ src/Mammatus.ServiceModel.Autofac/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mammatus.ServiceModel.Wcf.Autofac
+namespace Mammatus.ServiceModel.Autofac
 {
     public class Class1
     {
--- src/Mammatus.ServiceModel.Autofac/Properties/AssemblyInfo.cs
+++ src/Mammatus.ServiceModel.Autofac/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mammatus.ServiceModel.Wcf.Autofac")]
+[assembly: AssemblyTitle("Mammatus.ServiceModel.Autofac")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mammatus.ServiceModel.Wcf.Autofac")]
+[assembly: AssemblyProduct("Mammatus.ServiceModel.Autofac")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("27e8809d-9106-4bab-993d-0e54249a6c97")]
+[assembly: Guid("289ef3c0-a713-4613-9623-edb50405794e")]
 
 // Version information for an assembly consists of the following four values:
 //
