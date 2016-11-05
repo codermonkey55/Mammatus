f92643eddac31cf9b45117ad50450b6c050abb16
devworker55 <omnis.devworks@gmail.com>
2016-11-04 20:59:57 -0400

    Added new "StackTraceHelper" class.

---
+++ src/Mammatus/Helpers/StackTraceHelper.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace Mammatus.Helpers
+{
+    public class StackTraceHelper
+    {
+        private const int stackOffSet = 2;
+
+        public StackTraceHelper()
+        {
+
+        }
+
+        public MethodBase GetCallingMethod()
+        {
+            //-> Get current executing call stack.
+            StackTrace stackTrace = new StackTrace();
+
+            //-> Get calling method.
+            return stackTrace.GetFrame(stackOffSet).GetMethod();
+        }
+
+        public MethodBase GetMethodAt(int methodIndex)
+        {
+            methodIndex = stackOffSet + methodIndex;
+
+            //-> Get current executing call stack.
+            StackTrace stackTrace = new StackTrace();
+
+            // Get calling method.
+            return stackTrace.GetFrame(methodIndex).GetMethod();
+        }
+
+        public bool HasMethod(string methodName)
+        {
+            int methodIndex;
+
+            return HasMethod(methodName, out methodIndex);
+        }
+
+        public bool HasMethod(string methodName, out int methodIndex)
+        {
+            methodIndex = stackOffSet;
+
+            var stackTrace = new StackTrace();
+            var frameCount = stackTrace.FrameCount;
+            var hasMethod = false;
+
+            while (methodIndex < frameCount)
+            {
+                hasMethod = stackTrace.GetFrame(methodIndex)
+                                        .GetMethod()
+                                        .Name.Equals(methodName, System.StringComparison.CurrentCultureIgnoreCase);
+
+                if (hasMethod)
+                    break;
+                else
+                    methodIndex += 1;
+            }
+
+            if (hasMethod)
+                methodIndex -= stackOffSet;
+            else
+                methodIndex = 0;
+
+            return hasMethod;
+        }
+    }
+}
