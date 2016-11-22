14616578914794b80dc3442824ac63c1eab12b2a
devworker55 <omnis.devworks@gmail.com>
2016-11-22 18:24:11 -0500

    Added new Web state management components.

---
+++ src/Mammatus.Web/CookieHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web;
+
+namespace Mammatus.Web
+{
+    public static class CookieHelper
+    {
+        public static void ClearCookie(string cookiename)
+        {
+            HttpCookie cookie = HttpContext.Current.Request.Cookies[cookiename];
+            if (cookie != null)
+            {
+                cookie.Expires = DateTime.Now.AddYears(-3);
+                HttpContext.Current.Response.Cookies.Add(cookie);
+            }
+        }
+
+        public static string GetCookieValue(string cookiename)
+        {
+            HttpCookie cookie = HttpContext.Current.Request.Cookies[cookiename];
+            string str = string.Empty;
+            if (cookie != null)
+            {
+                str = cookie.Value;
+            }
+            return str;
+        }
+
+        public static void SetCookie(string cookiename, string cookievalue)
+        {
+            SetCookie(cookiename, cookievalue, DateTime.Now.AddDays(1.0));
+        }
+
+        public static void SetCookie(string cookiename, string cookievalue, DateTime expires)
+        {
+            HttpCookie cookie = new HttpCookie(cookiename)
+            {
+                Value = cookievalue,
+                Expires = expires
+            };
+            HttpContext.Current.Response.Cookies.Add(cookie);
+        }
+    }
+}
